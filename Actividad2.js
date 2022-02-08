0a971e20daf837beb41cbd4c234925009773883d
Marvin Lazaro <edu.canche@gmail.com>
2022-02-07 18:59:08 -0600

    First Commit

---
+++ Actividad2.js
@@ -0,0 +1,42 @@
+const nombre="Marvin Eduardo Lázaro Lemus"
+const carnet="22000638"
+const mes=10;
+switch (mes){
+    case 1:
+        alert("Es Invierno");
+        break;
+    case 2:
+        alert("Es Invierno");
+        break;
+    case 3:
+        alert("Es Primavera");
+        break;
+    case 4:
+        alert("Es Primavera");
+        break;
+    case 5:
+        alert("Es Primavera");
+    case 6:
+        alert("Es Verano");
+        break;
+    case 7:
+        alert("Es Verano");
+        break;
+    case 8:
+        alert("Es Verano");
+        break;
+    case 9:
+        alert("Es Otoño");
+        break;
+    case 10:
+        alert("Es Otoño")
+        break;
+    case 11:
+        alert("Es Otoño");
+        break;
+    case 12:
+        alert("Es Invierno");
+        break;
+}
+alert(nombre);
+alert(carnet);
